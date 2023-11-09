f1fbc6a196cdbfaa5c9f06730a036c788fe4f79c
Kristo <Kangrokristo1@gmail.com>
2023-11-09 09:03:30 +0200

    Update phpmyadmin_paigaldus.sh

--- phpmyadmin_paigaldus.sh
+++ phpmyadmin_paigaldus.sh
@@ -1,26 +1,47 @@
-#!/bin/bash
-# Kontrollime, kas skripti käivitaja on root kasutaja
-if [ "$EUID" -ne 0 ]
-  then echo "Palun käivita skript root kasutajana"
-  exit
-fi
+---
+- hosts: webservers
+  tasks:
+    - name: debconf for pma
+      debconf: name=phpmyadmin question='phpmyadmin/dbconfig-install' value='true' vtype='boolean'
 
-# Paigaldame vajalikud paketid (Apache, PHP ja phpMyAdmin)
-echo "Paigaldame vajalikud paketid..."
-apt-get update
-apt-get -y install apache2 php php-mysql phpmyadmin
+    - name: debconf for pma
+      debconf: name=phpmyadmin question='phpmyadmin/app-password-confirm' value='qwerty' vtype='password'
 
-# Konfigureerime phpMyAdmini teenuse
-echo "Konfigureerime phpMyAdmini..."
-ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
-a2enconf phpmyadmin
-service apache2 restart
+    - name: debconf for pma
+      debconf: name=phpmyadmin question='phpmyadmin/mysql/admin-pass' value='qwerty' vtype='password'
 
-# Kontrollime, kas paigaldamine õnnestus
-if [ $? -eq 0 ]; then
-    echo "phpMyAdmin on edukalt paigaldatud ja konfigureeritud!"
-else
-    echo "phpMyAdmini paigaldamisel ilmnes viga. Palun kontrolli süsteemi logisid."
-fi
+    - name: debconf for pma
+      debconf: name=phpmyadmin question='phpmyadmin/mysql/app-pass' value='qwerty' vtype='password'
 
-# Skripti lõpp
+    - name: debconf for pma
+      debconf: name=phpmyadmin question='phpmyadmin/reconfigure-webserver' value='' vtype='multiselect'
+
+    - name: Paigaldan phpmyadmin-5.2.1
+      unarchive:
+        src: https://files.phpmyadmin.net/phpMyAdmin/5.2.1/phpMyAdmin-5.2.1-all-languages.tar.gz
+        dest: /var/www/html/
+        remote_src: yes
+
+    - name: Muudan kausta nime 'phpmyadmin'
+      command: mv /var/www/html/phpMyAdmin-5.2.1-all-languages/ /var/www/html/kristo/
+
+    - name: Kopeerin konfiguratsioonifaili
+      command: cp /var/www/html/phpmyadmin/config.sample.inc.php /var/www/html/kristo/config.inc.php
+
+    - name: Lisan konfiguratsioonifaili kõigile lugemisõiguse
+      command: chmod 664 /var/www/html/kristo/config.inc.php
+
+    - name: Teen apache2 taaskäivituse
+      command: systemctl restart apache2
+
+    - name: Paigaldan php-mbstring
+      apt: name=php-mbstring update_cache=yes state=latest
+
+    - name: Määran kausta 'phpmyadmin' omaniku ja grupi 'www-data'
+      file:
+        path: /var/www/html/kristo/
+        state: directory
+        recurse: yes
+        owner: www-data
+        group: www-data
+
