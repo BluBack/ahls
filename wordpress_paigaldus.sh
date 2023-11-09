c03508d47f3135601a33fa28ec0b1844c8670ad1
Kristo <Kangrokristo1@gmail.com>
2023-11-09 09:04:12 +0200

    Update wordpress_paigaldus.sh

--- wordpress_paigaldus.sh
+++ wordpress_paigaldus.sh
@@ -1,40 +1,44 @@
-#!/bin/bash
-# Kontrollime, kas skripti käivitaja on root kasutaja
-if [ "$EUID" -ne 0 ]; then
-    echo "Palun käivita skript root kasutajana"
-    exit
-fi
+---
+- hosts: webservers
+  become: yes
+  become_user: root
+  tasks:
 
-# Kontrollime, kas apache_paigaldus.sh, php_paigaldus.sh ja pma_paigaldus.sh on olemas ja käivitatavad
-if [ ! -x "./apache_paigaldus.sh" ] || [ ! -x "./php_paigaldus.sh" ] || [ ! -x "./pma_paigaldus.sh" ]; then
-    echo "Vajalikud skriptid (apache_paigaldus.sh, php_paigaldus.sh, pma_paigaldus.sh) puuduvad või pole käivitatavad. Palun veendu, et need skriptid on olemas ja käivitatavad."
-    exit
-fi
+  - name: Loome andmebaas wordpressi jaoks wpdatabase
+    mysql_db:
+      name: wpdatabase
+      state: present
 
-# Kontrollime, kas vajalikud teenused on paigaldatud
-dpkg -l apache2 php phpmyadmin > /dev/null 2>&1
-apache_installed=$?
-php_installed=$?
-pma_installed=$?
+  - name: loome kasutaja wpuser ja maarame parool qwerty koos koikide privi
+    mysql_user:
+      name: root
+      password: qwerty
+      priv: 'wpdatabase.*:ALL,GRANT'
+      state: present
+      check_implicit_admin: yes
 
-if [ $apache_installed -ne 0 ] || [ $php_installed -ne 0 ] || [ $pma_installed -ne 0 ]; then
-    echo "Apache, PHP või phpMyAdmin teenus ei ole korralikult paigaldatud. Palun paigalda need teenused enne WordPressi paigaldamist."
-    exit
-fi
+  - name: alglaadmine mysql
+    service: name=mysql state=restarted
 
-# Paigaldame WordPressi
-echo "Paigaldame WordPressi..."
-wget https://wordpress.org/latest.tar.gz
-tar -xvf latest.tar.gz -C /var/www/html/
-chown -R www-data:www-data /var/www/html/wordpress
-chmod -R 755 /var/www/html/wordpress
-cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
+  - name: laen alla ja pakin lahti wordi
+    unarchive:
+      src: https://wordpress.org/latest.tar.gz
+      dest: /var/www/html/
+      remote_src: yes
 
-# Genereerime juhusliku salasõna WordPressi konfiguratsioonifaili jaoks
-salasona=$(openssl rand -base64 12)
-sed -i "s/define('DB_PASSWORD', '');/define('DB_PASSWORD', '$salasona');/" /var/www/html/wordpress/wp-config.php
+  - name: Vahetame wordpress kausta omanikku
+    file:
+      path: /var/www/html/wordpress/
+      state: directory
+      recurse: yes
+      owner: www-data
 
-echo "WordPress on edukalt paigaldatud ja seadistatud!"
-echo "Järgmine samm on WordPressi konfigureerimine veebibrauseris."
+  - name: kopeerime config faili
+    command: cp /var/www/html/wordpress/wp-config-sample.php /var/www/html/wordpress/wp-config.php
 
-# Skripti lõpp
+  - name: Vahetame DB_PASSWORD
+    lineinfile: dest=/var/www/html/wordpress/wp-config.php regexp="password_here" line="define( 'DB_PASSWORD', 'qwer$
+  - name: Vahetame DB_USER
+    lineinfile: dest=/var/www/html/wordpress/wp-config.php regexp="username_here" line="define( 'DB_USER', 'root' );"
+  - name: Vahetame DB_NAME
+    lineinfile: dest=/var/www/html/wordpress/wp-config.php regexp="database_name_here" line="define( 'DB_NAME', 'wpd$
